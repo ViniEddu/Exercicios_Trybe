e4168bfa06704129beb39b1541315cddf39983d9
Vinicius Eduardo <vinicius.eddu99@gmail.com>
2022-06-21 12:20:05 -0300

    exercicios de fixação dia 2

--- Front-End/exercicio_5/fixation-exercises-10-2/src/App.js
+++ Front-End/exercicio_5/fixation-exercises-10-2/src/App.js
@@ -1,37 +1,14 @@
 // arquivo App.js, criado pelo create-react-app, modificado
 import React from 'react';
-import './App.css';
-import Order from './Order';
+import Greeting from './Components/Greeting';
 
 class App extends React.Component {
   render() {
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
-      }
-    };
-
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
-
     return (
-      <div className="App">
-        <h1> Orders recently created </h1>
-         <Order order={energyDrink} />
-         <Order order={headphone} />         
-      </div>
-    );
+      <>
+        <Greeting name='Vinicius' lastName='Eduardo' />
+      </>
+    )
   }
 }
 
---
+++ Front-End/exercicio_5/fixation-exercises-10-2/src/Components/Greeting.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Greeting extends React.Component {
+    render() {
+        return(
+            <h1>Eae, {this.props.name} {this.props.lastName} </h1>
+        ) 
+    }
+}
+
+Greeting.propTypes = {
+    name: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired
+};
+
+export default Greeting;
